27e9ec1ecd9d76d415a0e1d1ac2b69b278db861e
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2025-01-20 23:13:01 +0100

    Make og-image generation a bit more secure

--- src/pages/api/og-image.ts
+++ src/pages/api/og-image.ts
@@ -1,24 +1,36 @@
-import puppeteer from 'puppeteer'
+import puppeteer, { Browser } from 'puppeteer'
 import type { APIRoute } from 'astro'
 
-export const GET: APIRoute = async (context) => {
-    const { searchParams } = new URL(context.request.url)
-    const url = searchParams.get('url')
+let browser: Browser | null = null
 
-    if (!url) {
+export const GET: APIRoute = async (context) => {
+    const { origin, searchParams } = new URL(context.request.url)
+    const target = searchParams.get('url')
+    if (!target) {
         return new Response('No URL provided', { status: 400 })
     }
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox'],
-    })
+    if (!browser) {
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox'],
+        })
+    }
+
     const page = await browser.newPage()
     await page.setViewport({ width: 1920, height: 1080 })
-    await page.goto(url)
+
+    // Ensure the target URL is safe and secure
+    // We make sure to use the origin of the request URL
+    const targetUrl = new URL(target)
+    const { pathname: targetPath, searchParams: targetSearchParams } = targetUrl
+    const safeUrl = `${origin}${targetPath}${targetSearchParams.toString()}`
+
+    await page.goto(safeUrl)
     await page.evaluate(() => {
         document.body.style.zoom = '0.65'
     })
+
     const imageBuffer = await page.screenshot({ type: 'png' })
     await browser.close()
 
