4e2345ff5d8c44ab17fb8352678fe151e1701f22
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2025-04-02 17:17:08 +0200

    Use passthrough image service for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
@@ -22,4 +22,7 @@ export default defineConfig({
     adapter: node({
         mode: 'standalone',
     }),
+    image: {
+        service: passthroughImageService(),
+    },
 })
