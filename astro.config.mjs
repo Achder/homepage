e6e0d6b65feed3e6fcf29d17dfe51ac4ad00cb27
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2026-04-02 11:37:13 +0200

    Updated deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig, passthroughImageService, fontProviders } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import node from '@astrojs/node'
@@ -10,7 +9,7 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [mdx(), sitemap()],
+    integrations: [sitemap()],
     output: 'static',
     prefetch: {
         prefetchAll: true,
@@ -25,33 +24,31 @@ export default defineConfig({
     image: {
         service: passthroughImageService(),
     },
-    experimental: {
-        fonts: [
-            {
-                provider: fontProviders.google(),
-                name: 'Rowdies',
-                cssVariable: '--font-rowdies',
-            },
-            {
-                provider: fontProviders.google(),
-                name: 'Cal Sans',
-                cssVariable: '--font-cal-sans',
-            },
-            {
-                provider: fontProviders.google(),
-                name: 'Bebas Neue',
-                cssVariable: '--font-bebas-neue',
-            },
-            {
-                provider: fontProviders.google(),
-                name: 'Pacifico',
-                cssVariable: '--font-pacifico',
-            },
-            {
-                provider: fontProviders.google(),
-                name: 'Caveat',
-                cssVariable: '--font-caveat',
-            },
-        ],
-    },
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: 'Rowdies',
+            cssVariable: '--font-rowdies',
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Cal Sans',
+            cssVariable: '--font-cal-sans',
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Bebas Neue',
+            cssVariable: '--font-bebas-neue',
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Pacifico',
+            cssVariable: '--font-pacifico',
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Caveat',
+            cssVariable: '--font-caveat',
+        },
+    ],
 })
